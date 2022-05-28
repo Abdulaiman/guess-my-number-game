8ce3ede8cc6d14ce6812dfdf6556fa60df42d769
rabiu abdulazeez <rabiuabdulazeez@rabius-MacBook.local>
2022-05-28 13:36:25 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const secNumber = Math.trunc(Math.random() * 100) + 1;
+console.log(secNumber);
+
+let score = 20;
+let highScore = 0;
+const mainFunction = (classs, messsage) => {
+  document.querySelector(classs).textContent = messsage;
+};
+const userName = prompt('input your name here');
+document.querySelector('.check').addEventListener('click', function () {
+  const checkk = Number(document.querySelector('.guess').value);
+  if (!checkk) {
+    mainFunction('.message', 'No Number Selected');
+  } else if (checkk === secNumber) {
+    mainFunction('.message', `${userName} won the game....`);
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    mainFunction('.number', secNumber);
+    if (score > highScore) {
+      highScore = score;
+      mainFunction('.highscore', highScore);
+    }
+  } else if (checkk !== secNumber) {
+    if (score > 1) {
+      mainFunction(
+        '.message',
+        checkk > secNumber ? 'too high.....' : 'too low.....'
+      );
+      score--;
+      mainFunction('.score', score);
+    } else {
+      mainFunction('.score', 0);
+      mainFunction('.message', 'you lost the game');
+      document.querySelector('body').style.backgroundColor = '#ff0000';
+    }
+  }
+});
+document.querySelector('.again').addEventListener('click', function () {
+  mainFunction('.message', 'start guessing');
+  score = 20;
+  mainFunction('.score', 20);
+
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  mainFunction('.number', '?');
+  document.querySelector('.guess').value = '';
+});
